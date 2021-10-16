aea2072e08afb751f034ac43e6be47d5819d7ca4
Luis Rovirosa Mairlot <luis@codium.team>
2021-10-16 13:21:20 +0200

    [refactor] cheap_drink_maker_adapter uses _drink for chocolate

--- coffee_machine/infrastructure/cheap_drink_maker_adapter.py
+++ coffee_machine/infrastructure/cheap_drink_maker_adapter.py
@@ -26,11 +26,11 @@ class CheapDrinkMakerAdapter(DrinkMaker):
                 self._prepare_drink(f"{self._drink(drink)}:2:0")
         else:
             if drink.sugar == 0:
-                self._prepare_drink(f"H::")
+                self._prepare_drink(f"{self._drink(drink)}::")
             elif drink.sugar == 1:
-                self._prepare_drink(f"H:1:0")
+                self._prepare_drink(f"{self._drink(drink)}:1:0")
             elif drink.sugar == 2:
-                self._prepare_drink(f"H:2:0")
+                self._prepare_drink(f"{self._drink(drink)}:2:0")
 
     def _prepare_drink(self, command):
        self.cheap_drink_maker.execute(command)
@@ -39,7 +39,8 @@ class CheapDrinkMakerAdapter(DrinkMaker):
     def _drink(drink: Drink):
         drink_codes = {
             DrinkType.Coffee: 'C',
-            DrinkType.Tea: 'T'
+            DrinkType.Tea: 'T',
+            DrinkType.Chocolate: 'H',
         }
         return drink_codes[drink.drink]
 
