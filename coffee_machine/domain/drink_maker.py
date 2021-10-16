532bfda6c0b6f7028475fd94a07dac28d45d62ec
Luis Rovirosa Mairlot <luis@codium.team>
2021-10-16 12:58:42 +0200

    [refactor] add drink_maker as "interface"

---
+++ coffee_machine/domain/drink_maker.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+from coffee_machine.domain.drink import Drink
+
+
+class DrinkMaker(ABC):
+    @abstractmethod
+    def prepare(self, drink: Drink):
+        raise NotImplemented()
