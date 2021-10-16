cd0a171520ff6affa1c514467273287dcf616755
Luis Rovirosa Mairlot <luis@codium.team>
2021-10-16 14:55:56 +0200

    [refactor] price_list has prices initialized in constructor

--- coffee_machine/domain/price_list.py
+++ coffee_machine/domain/price_list.py
@@ -2,10 +2,12 @@ from coffee_machine.domain.drink_type import DrinkType
 
 
 class PriceList:
-    def price_of(self, drink_type: DrinkType) -> int:
-        prices = {
+    def __init__(self):
+        self.prices = {
             DrinkType.Coffee: 60,
             DrinkType.Tea: 40,
             DrinkType.Chocolate: 50,
         }
-        return prices.get(drink_type)
+
+    def price_of(self, drink_type: DrinkType) -> int:
+        return self.prices.get(drink_type)
