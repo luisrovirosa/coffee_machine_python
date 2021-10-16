ce68500e6200eae21503b44b544b625081674ed6
Luis Rovirosa Mairlot <luis@codium.team>
2021-10-16 19:53:42 +0200

    test_cheap_drink_maker_adapter parametrized

--- tests/test_cheap_drink_maker_adapter.py
+++ tests/test_cheap_drink_maker_adapter.py
@@ -1,3 +1,4 @@
+import pytest
 from doublex import Spy
 from doublex_expects import have_been_called_with
 from expects import expect
@@ -9,10 +10,13 @@ from coffee_machine.infrastructure.cheap_drink_maker_adapter import CheapDrinkMa
 
 
 class TestCheapDrinkMakerAdapter:
-    def test_adapt_different_types_of_drinks(self):
+    @pytest.mark.parametrize('drink,expected_command',[
+        (Drink(DrinkType.Coffee, 0), 'C::')
+    ])
+    def test_adapt_different_types_of_drinks(self, drink: Drink, expected_command: str):
         cheap_drink_maker = Spy(CheapDrinkMaker)
         adapter = CheapDrinkMakerAdapter(cheap_drink_maker)
 
-        adapter.prepare(Drink(DrinkType.Coffee, 0))
+        adapter.prepare(drink)
 
-        expect(cheap_drink_maker.execute).to(have_been_called_with('C::'))
+        expect(cheap_drink_maker.execute).to(have_been_called_with(expected_command))
