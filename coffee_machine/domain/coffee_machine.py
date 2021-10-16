b5b65d184122e249a9c2a3a06c3b1930270b1260
Luis Rovirosa Mairlot <luis@codium.team>
2021-10-16 13:15:11 +0200

    [refactor] reduce complexity on coffee_machine.py

--- coffee_machine/domain/coffee_machine.py
+++ coffee_machine/domain/coffee_machine.py
@@ -6,7 +6,6 @@ from coffee_machine.infrastructure.cheap_drink_maker_adapter import CheapDrinkMa
 
 class CoffeeMachine:
     def __init__(self, drink_maker: CheapDrinkMaker) -> None:
-        self.drink_maker = drink_maker
         self.drink_maker_adapter = CheapDrinkMakerAdapter(drink_maker)
         self.sugar_level = 0
 
