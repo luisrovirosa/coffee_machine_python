acda04665d91a9c73ad46f9d8be0505ef8700972
Luis Rovirosa Mairlot <luis@codium.team>
2021-10-17 11:13:31 +0200

    [refactor] PriceList more pythonic way?

--- coffee_machine/domain/price_list.py
+++ coffee_machine/domain/price_list.py
@@ -3,9 +3,8 @@ from coffee_machine.domain.drink_type import DrinkType
 
 class PriceList:
     def __init__(self, prices: dict):
-        for drink_type in DrinkType:
-            if drink_type not in prices:
-                raise Exception('Missing price for {type}')
+        if not all(drink_type in prices for drink_type in DrinkType):
+            raise Exception(f'Missing price for {type}')
         self.prices = prices
 
     def price_of(self, drink_type: DrinkType) -> int:
