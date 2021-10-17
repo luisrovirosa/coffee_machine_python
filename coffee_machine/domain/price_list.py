6ed2a410a4078d175f29ff8e7816eb6310d6cf81
Luis Rovirosa Mairlot <luis@codium.team>
2021-10-17 11:11:41 +0200

    fail_when_missing_price_for_a_product

--- coffee_machine/domain/price_list.py
+++ coffee_machine/domain/price_list.py
@@ -3,6 +3,9 @@ from coffee_machine.domain.drink_type import DrinkType
 
 class PriceList:
     def __init__(self, prices: dict):
+        for drink_type in DrinkType:
+            if drink_type not in prices:
+                raise Exception('Missing price for {type}')
         self.prices = prices
 
     def price_of(self, drink_type: DrinkType) -> int:
--- tests/domain/test_price_list.py
+++ tests/domain/test_price_list.py
@@ -1,13 +1,21 @@
+from expects import raise_error, expect
+
 from coffee_machine.domain.drink_type import DrinkType
 from coffee_machine.domain.price_list import PriceList
 
 
 class TestPriceList:
-    price_for_all_drinks = {
-        DrinkType.Coffee: 1,
-        DrinkType.Tea: 1,
-        DrinkType.Chocolate: 1,
-    }
+    def setup(self):
+        self.price_for_all_drinks = {
+            DrinkType.Coffee: 1,
+            DrinkType.Tea: 1,
+            DrinkType.Chocolate: 1,
+        }
 
     def test_can_be_created_with_all_drink_type_prices(self):
         PriceList(self.price_for_all_drinks)
+
+    def test_fail_when_missing_price_for_a_product(self):
+        del(self.price_for_all_drinks[DrinkType.Coffee])
+
+        expect(lambda : PriceList(self.price_for_all_drinks)).to(raise_error)
